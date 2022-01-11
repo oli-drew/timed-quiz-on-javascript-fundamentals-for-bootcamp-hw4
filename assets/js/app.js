582397f116961e1a292abb924ab6c0e19f70b2bb
Oli Drew <oli.drew92@gmail.com>
2022-01-11 15:57:40 +0000

    List of requirements added to app.js and html modified

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
 console.log("File linked!");
+
+// Timer - 5 minutes
+
+// Button to start the quiz
+
+// Local storage for leader board
+
+// 10 Questions - multiple choice - 4 answers
+
+// Correct answer = next question
+
+// Incorrect answer = time subtracted from timer (20s)
+
+// Game finishes when all questions answered or timer = 0
+
+// User is presented with their score
+
+// User can enter initials into form to save score to leader board
