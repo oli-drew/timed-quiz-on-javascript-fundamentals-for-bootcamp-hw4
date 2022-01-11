946571bd2cf8cabce979ea93ec04010037016472
Oli Drew <oli.drew92@gmail.com>
2022-01-11 16:14:39 +0000

    Create js variables and outline of required functions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,46 @@
 console.log("File linked!");
 
 // Timer - 5 minutes
+const timerStart = 300;
+// Timer element
+const timerEl = document.querySelector("timer");
+// Timer function
+const timer = () => {
+  //
+};
 
 // Button to start the quiz
+const startBtn = document.querySelector("#startBtn");
 
 // Local storage for leader board
 
-// 10 Questions - multiple choice - 4 answers
+// 10 Questions - multiple choice - 4 answers - JS object
+const questions = {};
+
+// Check Answer
+const checkAnswer = () => {
+  //
+};
 
 // Correct answer = next question
+const correctAnswer = () => {
+  //
+};
 
 // Incorrect answer = time subtracted from timer (20s)
+const incorrectAnswer = () => {
+  //
+};
 
 // Game finishes when all questions answered or timer = 0
+const gameOver = () => {
+  //
+};
 
 // User is presented with their score
 
 // User can enter initials into form to save score to leader board
+const submitScore = () => {};
+
+// Submit score button
+const submitScoreBtn = document.querySelector("#submitScore");
