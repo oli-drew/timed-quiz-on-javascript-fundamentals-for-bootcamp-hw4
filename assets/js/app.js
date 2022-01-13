4111f72ecd7aaf0cb1e8d9cad212117cced9ccf8
Oli Drew <oli.drew92@gmail.com>
2022-01-13 11:07:13 +0000

    Dynamically create answer option buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,84 +1,104 @@
 "use strict";
-// 10 Questions - multiple choice - 4 answers - JS object
+// 10 Questions - multiple choice - 4 answers - JS objects within an array
 const questions = [
   {
-    question: "Q1: Question One?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    question: "Q1: What is JS an abbreviation for?",
+    answers: {
+      a: "JavaScript",
+      b: "Java Source",
+      c: "Just Sing",
+      d: "Jeremy Slarkson",
+    },
     correct: "a",
   },
   {
     question: "Q2: Question Two?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "b",
   },
   {
     question: "Q3: Question Three?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "a",
   },
   {
     question: "Q4: Question Four?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "d",
   },
   {
     question: "Q5: Question Five?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "c",
   },
   {
     question: "Q6: Question Six?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "b",
   },
   {
     question: "Q7: Question Seven?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "d",
   },
   {
     question: "Q8: Question Eight?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "d",
   },
   {
     question: "Q9: Question Nine?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "b",
   },
   {
     question: "Q10: Question Ten?",
-    a: "Answer 1",
-    b: "Answer 2",
-    c: "Answer 3",
-    d: "Answer 4",
+    answers: {
+      a: "Answer 1",
+      b: "Answer 2",
+      c: "Answer 3",
+      d: "Answer 4",
+    },
     correct: "c",
   },
 ];
@@ -94,9 +114,6 @@ let questionNumber = 0;
 
 // Question element
 const questionElement = document.querySelector("#question");
-
-// Question options
-const optionButtons = document.querySelectorAll("#optionBtn");
 
 // Timer - 5 minutes = 300 secs
 const timerStart = 300;
@@ -143,16 +160,29 @@ const getQuestion = () => {
   // Change dom element with question
   questionElement.textContent = questions[questionNumber].question;
 
-  // --- Might use a loop for this later --
-  // optionButtons.forEach((btn) => {
-  //   btn.textContent = questions[questionNumber].b;
-  // });
+  // Turns answers object keys and values into array so we can iterate over
+  const answers = Object.entries(questions[questionNumber].answers);
 
-  // Change dom element text with options
-  optionBtn[0].textContent = questions[questionNumber].a;
-  optionBtn[1].textContent = questions[questionNumber].b;
-  optionBtn[2].textContent = questions[questionNumber].c;
-  optionBtn[3].textContent = questions[questionNumber].d;
+  // Where to place option buttons
+  const optionsElement = document.querySelector("#options");
+
+  // Dynamically create option buttons
+  answers.forEach(([key, value]) => {
+    // Create the button  to insert
+    const optionBtn = document.createElement("button");
+    // Set button data-answer attribute
+    optionBtn.setAttribute("data-answer", key);
+    // Set button text
+    optionBtn.textContent = value;
+    // Add event listener to option button
+    optionBtn.addEventListener("click", (event) => {
+      let userAnswer = event.target.getAttribute("data-answer");
+      console.log(userAnswer);
+      checkAnswer(userAnswer);
+    });
+    // Append button
+    optionsElement.append(optionBtn);
+  });
 };
 
 // Check Answer
@@ -165,16 +195,6 @@ const checkAnswer = (answer) => {
   }
 };
 
-// Assign event listener to buttons
-optionButtons.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    // console.log(event.target.getAttribute("data-answer"));
-    let userAnswer = event.target.getAttribute("data-answer");
-    console.log(userAnswer);
-    checkAnswer(userAnswer);
-  });
-});
-
 // Correct answer = next question
 const correctAnswer = () => {
   console.log("Correct");
@@ -186,11 +206,15 @@ const correctAnswer = () => {
     console.log(currentScore);
     // Increase question number
     questionNumber++;
+    // Remove options
+    removeOptions();
     // Get next question
     getQuestion();
   } else {
     // Add to score for last question
     currentScore++;
+    // Remove answer options
+    removeOptions();
     console.log(currentScore);
     // Finish game
     gameOver();
@@ -203,6 +227,12 @@ const incorrectAnswer = () => {
   console.log("Wrong");
   //   Remove 20 secs from timer
   timeRemaining = timeRemaining - 20;
+};
+
+// Remove answer options
+const removeOptions = () => {
+  const options = document.querySelectorAll("#options button");
+  options.forEach((option) => option.remove());
 };
 
 // Game finishes when all questions answered or timer = 0
